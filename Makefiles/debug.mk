1d7f1f5296344ffe240cde191e69416b5961153c
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2025-09-09 19:14:09 +0000

    Fix

--- Makefiles/debug.mk
+++ Makefiles/debug.mk
@@ -5,7 +5,7 @@ include Makefiles/rules.mk
 DOCKER_FILE_PATH := dockerfiles/Dockerfile.debug
 
 DOCKER_BUILD_CMD := $(DOCKER_HOST) image build -t $(DOCKER_IMG_NAME) -f $(DOCKER_FILE_PATH) $(DOCKER_BUILD_CONTEXT)
-DOCKER_RUN_CMD := $(DOCKER_HOST) container run $(DOCKER_ARG) $(DOCKER_IMG_NAME)
+DOCKER_RUN_CMD := $(DOCKER_HOST) container run -e USER -e HOME $(DOCKER_ARG) $(DOCKER_IMG_NAME)
 
 # Define the default target
 .PHONY: all test clean
